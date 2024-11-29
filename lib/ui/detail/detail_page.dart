a8c7cd7e403dc5647a9b606ec7f5d18510fe67c5
Yoonsuh Kim <blawesomek@gmail.com>
2024-11-29 17:44:25 +0900

    detail_page

--- lib/ui/detail/detail_page.dart
+++ lib/ui/detail/detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class DetailPage extends StatelessWidget {
   @override
@@ -7,7 +8,15 @@ class DetailPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Harry Potter and the Philosopher's Stone"),
       ),
-      body: Text('DetailPage'),
+      body: InAppWebView(
+        initialUrlRequest: URLRequest(
+          url: WebUri('https://www.naver.com'),
+        ),
+        initialSettings: InAppWebViewSettings(
+          userAgent:
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+        ),
+      ),
     );
   }
 }
